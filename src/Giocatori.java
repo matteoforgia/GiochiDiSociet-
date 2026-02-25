f5949c7bdc6548773986b245a2605bc280989193
matteoforgia <matteo.forgiarini28@gmail.com>
2026-02-25 19:16:25 +0100

    creando la classe Gioco

--- src/Giocatori.java
+++ src/Giocatori.java
@@ -1,18 +1,23 @@
+import java.util.Random;
 import java.util.UUID;
 
 public class Giocatori {
         private String nome;
         private int partiteVinte;
-    
+        private int punteggio;
+        
         public Giocatori() {
             this.nome = UUID.randomUUID().toString().substring(0, 5);
             this.partiteVinte = 0;
+            this.punteggio = rand.nextInt(10);
         }
+
+        Random rand = new Random();
 
         public String getNome() {
             return nome;
         }
-
+        
         public int getPartiteVinte() {
             return partiteVinte;
         }
@@ -20,5 +25,5 @@ public class Giocatori {
         public void setPartiteVinte(int partiteVinte) {
             this.partiteVinte = partiteVinte;
         }
-        
+
 }
---
+++ src/Gioco.java
@@ -0,0 +1,10 @@
+public class Gioco {
+    Giocatori[] giocatori = new Giocatori[2];
+    int ultimoVincitore;
+
+    public void Gioca() {
+        for (int i = 0; i < giocatori.length; i++) {
+            
+        }
+    }
+}
